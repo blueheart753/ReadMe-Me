8b15360ef8a267a1680bcaf0fe300640adcb5bfc
kimminam <bluemind8520@gmail.com>
2024-04-27 21:56:43 +0900

    Not changed

--- read-me-me/src/app/page.tsx
+++ read-me-me/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Profile from './components/profile';
+import Profile from './components/profile'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-14">
       <Profile />
     </main>
-  );
+  )
 }
