83b7d88ee569c323d33cff06c9bd8057024dcae2
kimminam <bluemind8520@gmail.com>
2024-04-27 21:57:45 +0900

    add OpenWeatherMap API connect openweathermap api and import weather data

--- read-me-me/src/app/components/profile.tsx
+++ read-me-me/src/app/components/profile.tsx
@@ -1,6 +1,67 @@
+'use client'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
+interface WeatherRes {
+  main: {
+    temp: number
+    humidity: number
+  }
+  weather: {
+    description: string
+  }[]
+}
+
+const fetchWeather = async (
+  city: string,
+  apiKey: string
+): Promise<{
+  temperature: number
+  humidity: number
+  description: string
+}> => {
+  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+  try {
+    const response = await fetch(apiUrl)
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data')
+    }
+
+    const data: WeatherRes = await response.json()
+
+    return {
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+    }
+  } catch (error) {
+    throw new Error('Failed to fetch weather data')
+  }
+}
+
 const Profile = () => {
+  const [weatherData, setWeatherData] = useState<{
+    temperature: number
+    humidity: number
+    description: string
+  } | null>(null)
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const city = 'Seoul'
+        const apiKey = '329b6fdafb9a0bfcc3854da027c0b3c9'
+        const data = await fetchWeather(city, apiKey)
+        setWeatherData(data)
+      } catch (error) {
+        console.error('Error fetching weather data:')
+      }
+    }
+
+    fetchWeatherData()
+  }, [])
+
   return (
     <div className="flex flex-col items-center">
       <div className="sm:w-screen flex flex-col flex-wrap p-2 gap-2">
@@ -26,6 +87,7 @@ const Profile = () => {
             세상에 대한 호기심으로 어려움을 찾고 <br />
             문제를 해결을 위해 나아가는 개발자
           </p>
+          {weatherData?.description}
         </div>
       </div>
     </div>
