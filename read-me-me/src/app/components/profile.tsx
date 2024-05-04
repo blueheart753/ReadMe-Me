403357980475c371efb43900601e0ca80220ba6c
kimminam <bluemind8520@gmail.com>
2024-05-04 19:47:24 +0900

    delete second title

--- read-me-me/src/app/components/profile.tsx
+++ read-me-me/src/app/components/profile.tsx
@@ -6,16 +6,9 @@ const Profile = () => {
           안녕하세요!
         </p>
         <p className="line-clamp-2 text-3xl font-black text-center leading-relaxed sm:text-7xl">
-          제 이름은 <span className="whitespace-nowrap">김미남</span> 입니다!
+          제 이름은 <span className="whitespace-nowrap shadow-lg">김미남</span>{' '}
+          입니다!
         </p>
-      </div>
-      <div>
-        <div className="p-6">
-          <p className="font-semibold text-center">
-            세상에 대한 호기심으로 어려움을 찾고 <br />
-            문제를 해결을 위해 나아가는 개발자
-          </p>
-        </div>
       </div>
     </div>
   );
