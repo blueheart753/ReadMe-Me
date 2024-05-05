93a9b7701a593639e414471d9555294328ee78cf
kimminam <bluemind8520@gmail.com>
2024-05-06 03:09:09 +0900

    add text-stroke

--- read-me-me/src/app/components/profile.tsx
+++ read-me-me/src/app/components/profile.tsx
@@ -1,17 +1,18 @@
 const Profile = () => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="sm:w-screen flex flex-col flex-wrap p-2 gap-2 justify-center">
+    <div className="h-screen flex flex-col items-center justify-center mx-auto">
+      <div className="flex flex-col flex-wrap p-2 gap-2 justify-center ">
         <p className="line-clamp-2 text-3xl font-black text-center leading-relaxed sm:text-7xl">
           안녕하세요!
         </p>
         <p className="line-clamp-2 text-3xl font-black text-center leading-relaxed sm:text-7xl">
-          제 이름은 <span className="whitespace-nowrap shadow-lg">김미남</span>{' '}
+          제 이름은
+          <span className="whitespace-nowrap text-stroke"> 김미남 </span>
           입니다!
         </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
