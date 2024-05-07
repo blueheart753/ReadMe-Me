e985a57c9895a2f7f4f0f927a3c27ff27f2f2115
kimminam <bluemind8520@gmail.com>
2024-05-07 14:27:06 +0900

    changed css

--- read-me-me/src/app/components/Description.tsx
+++ read-me-me/src/app/components/Description.tsx
@@ -1,11 +1,11 @@
 const Description = (props: any) => {
-  const { title, description } = props
+  const { title, description } = props;
   return (
-    <div className="w-9/12 p-4 flex flex-col">
-      <p className="text-3xl text-end font-semibold">{title}</p>
-      <p className="text-xl text-end">{description}</p>
+    <div className="p-4 flex flex-col gap-2">
+      <p className="text-xl sm:text-3xl text-center font-semibold">{title}</p>
+      <p className="w-full text-md sm:text-xl text-center">{description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
