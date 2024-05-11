c782e0de260c33c878ea1dbf9664e53e15b10653
kimminam <bluemind8520@gmail.com>
2024-05-12 01:43:42 +0900

    not Changed

--- read-me-me/src/app/page.tsx
+++ read-me-me/src/app/page.tsx
@@ -1,11 +1,10 @@
-import Profile from './components/profile'
-import WhoIAm from './components/Who_I_am'
-
+import Profile from './components/profile';
+import WhoIAm from './components/Who_I_am';
 export default function Home() {
   return (
     <main>
       <Profile />
       <WhoIAm />
     </main>
-  )
+  );
 }
