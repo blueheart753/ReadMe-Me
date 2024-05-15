c6ab9f574314031e25319f5d91e9fb57453bd766
kimminam <bluemind8520@gmail.com>
2024-05-15 20:24:57 +0900

    add props Type and props

--- read-me-me/src/app/components/Description.tsx
+++ read-me-me/src/app/components/Description.tsx
@@ -1,10 +1,17 @@
-const Description = (props: { title: string; description: string }) => {
-  const { title, description } = props
+const Description = (props: {
+  title: string
+  description: React.ReactNode
+  subTitle: string
+}) => {
+  const { title, subTitle, description } = props
   return (
-    <div className="p-4 flex flex-col gap-2">
-      <p className="text-xl sm:text-3xl text-center font-semibold">{title}</p>
-      <p className="w-full text-md sm:text-xl text-center">{description}</p>
-    </div>
+    <article className="w-9/12 border-2 w-96 h-1/2 p-4 rounded-lg bg-slate-800">
+      <div className="p-2">
+        <h1 className="text-white text-4xl">{title}</h1>
+        <h3 className="text-gray-400">{subTitle}</h3>
+      </div>
+      <div className="text-gray-400 text-xl p-2">{description}</div>
+    </article>
   )
 }
 
