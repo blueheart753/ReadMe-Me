5219abcc42ba037f30fe8b16a3ccb982cdbba8f3
kimminam <bluemind8520@gmail.com>
2024-05-15 20:26:03 +0900

    changed div tag -> Semantic tag

--- read-me-me/src/app/components/profile.tsx
+++ read-me-me/src/app/components/profile.tsx
@@ -26,18 +26,18 @@ const Profile = () => {
   }, [])
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center mx-auto">
-      <div className="flex flex-col flex-wrap p-2 gap-2 justify-center ">
-        <p className="text-white line-clamp-2 text-3xl font-black text-center leading-relaxed sm:text-7xl">
+    <section className="h-screen flex flex-col items-center justify-center mx-auto">
+      <header className="flex flex-col flex-wrap p-2 gap-2 justify-center ">
+        <h1 className="text-white line-clamp-2 text-3xl font-black text-center leading-relaxed sm:text-7xl">
           안녕하세요!
-        </p>
+        </h1>
         <p className="text-white line-clamp-2 text-3xl font-black text-center leading-relaxed sm:text-7xl">
           제 이름은
           <span className="whitespace-nowrap text-stroke text-8xl">김미남</span>
           입니다!
         </p>
-      </div>
-    </div>
+      </header>
+    </section>
   )
 }
 
