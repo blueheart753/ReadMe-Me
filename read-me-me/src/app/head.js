537f68ea571bc06a76d92d5d2e41fedc2b542f6f
kimminam <bluemind8520@gmail.com>
2024-05-17 13:11:56 +0900

    changed seo and profile photo folder

---
+++ read-me-me/src/app/head.js
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>My Next.js PWA</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="Read-Me-Me" content="김미남의 자기소개 페이지" />
+      <link rel="manifest" href="/vmanifest.webmanifest" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="icon" href="/icons/icon-192x192.png" />
+      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+    </>
+  );
+}
