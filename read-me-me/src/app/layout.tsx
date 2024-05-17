ed8696a828f9348cc030f4db79c48dd739c14224
kimminam <bluemind8520@gmail.com>
2024-05-17 13:04:07 +0900

    SEO changed

--- read-me-me/src/app/layout.tsx
+++ read-me-me/src/app/layout.tsx
@@ -17,11 +17,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <meta name="keyword" content="Kimminam" />
-        <meta name="keyword" content="kimminam" />
-        <meta name="keyword" content="김미남" />
-        <meta name="keyword" content="Minam Kim" />
-        <meta name="Description" content="안녕하세요! 저는 김미남입니다." />
+        <meta
+          name="Description"
+          content="안녕하세요! 저는 김미남입니다., Minam Kim, 김미남, kimminam, Kimminam"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
