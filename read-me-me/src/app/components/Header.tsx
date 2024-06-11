386cdd60dc15eb4fc9730f92e7f541f91b3335b5
kimminam <bluemind8520@gmail.com>
2024-06-11 15:07:25 +0900

    Feat : 헤더 컨포넌트 생성

---
+++ read-me-me/src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import styles from '../page.module.css';
+
+const Header = () => {
+  return (
+    <section>
+      <div className={styles.titleBox}>
+        <h1 className={styles.titleName}>KimMinam</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
