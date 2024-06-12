cd184374d04518b87037770564f6e63819a5b936
kimminam <bluemind8520@gmail.com>
2024-06-12 15:07:45 +0900

    Add : Profile, Pros, Cons Components

--- read-me-me/src/app/page.tsx
+++ read-me-me/src/app/page.tsx
@@ -1,10 +1,20 @@
 import styles from './page.module.css';
 import Header from './components/Header';
+import Profile from './components/Profile';
+import Pros from './components/Pros';
+import Cons from './components/Cons';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
+      <section className={styles.sectionWrap}>
+        <Profile />
+        <section className={styles.prosConsWrap}>
+          <Pros />
+          <Cons />
+        </section>
+      </section>
     </main>
   );
 }
