49ba1f33f798227ef74bc64b763af54aeb92047b
kimminam <bluemind8520@gmail.com>
2024-06-12 09:01:15 +0900

    Add : ClassName titleWrap

--- read-me-me/src/app/components/Header.tsx
+++ read-me-me/src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import styles from '../page.module.css';
 
 const Header = () => {
   return (
-    <section>
+    <section className={styles.titleWrap}>
       <div className={styles.titleBox}>
         <h1 className={styles.titleName}>KimMinam</h1>
       </div>
