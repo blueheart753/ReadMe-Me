2626b55d9c02e54f1912210ec1238616bf6610c6
kimminam <bluemind8520@gmail.com>
2024-06-22 00:25:34 +0900

    seo 최적화

--- read-me-me/src/app/components/LikeDislike.tsx
+++ read-me-me/src/app/components/LikeDislike.tsx
@@ -1,14 +1,7 @@
-import styles from '../page.module.css';
+import styles from '../page.module.css'
 
 interface likeOrDislike {
-  isLike: boolean;
-}
-
-interface likeAndDislikeType {
-  title: string;
-  info: string;
-  subTitle: string;
-  description: string;
+  isLike: boolean
 }
 
 const LikeDislike = ({ isLike }: likeOrDislike) => {
@@ -16,30 +9,32 @@ const LikeDislike = ({ isLike }: likeOrDislike) => {
     title: 'Like.',
     subTitle: '좋아하는 것.',
     info: 'IT정보 찾기.',
-    desription: 'WWDC, 구글IO등 IT 이벤트를 찾아보고 참여하는 것을 좋아합니다.',
-  };
+    desription:
+      'WWDC, 구글IO 등 IT와 관련된 이벤트를 찾아보고 참여하는 것을 좋아합니다. 또한 새로운 제품을 찾고 적용함으로써 만족감을 느끼고 있습니다.',
+  }
 
   const DisLikeProps = {
     title: 'Dislike.',
     subTitle: '싫어하는 것.',
     info: '자신의 주장만 펼치는 것.',
-    desription: '근거 없이 자신의 주장만 펼치는 것을 아주 싫어합니다.',
-  };
+    desription:
+      '아무런 근거 없이 자신의 주장만 펼치는 것과 타인의 의견을 무시하는 것을 아주 싫어합니다.',
+  }
 
-  const currentProps = isLike ? LikeProps : DisLikeProps;
+  const currentProps = isLike ? LikeProps : DisLikeProps
 
   return (
-    <section>
-      <div className={styles.descriptionWrap}>
+    <section className={styles.likeDescriptionWrap}>
+      <div>
         <p className={styles.title}>{currentProps.title}</p>
         <p className={styles.info}>{currentProps.subTitle}</p>
-        <div className={styles.descriptionBox}>
-          <p className={styles.subTitle}>{currentProps.info}</p>
-          <p className={styles.description}>{currentProps.desription}</p>
-        </div>
+      </div>
+      <div className={styles.onlyDescriptionBox}>
+        <p className={styles.subTitle}>{currentProps.info}</p>
+        <p className={styles.description}>{currentProps.desription}</p>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default LikeDislike;
+export default LikeDislike
