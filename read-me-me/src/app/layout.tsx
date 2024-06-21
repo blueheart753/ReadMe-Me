af6a3bc63405f2386f050ebea83f3093270ddd76
kimminam <bluemind8520@gmail.com>
2024-06-22 01:19:03 +0900

    seo google search 최적화

--- read-me-me/src/app/layout.tsx
+++ read-me-me/src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Default_Mi-nam',
-  description: 'Gimminam ',
+  description: '안녕하세요! 저는 김미남이라고 합니다!',
 }
 
 export default function RootLayout({
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="IRNnpCop6wOd0Tn9nBhgPeHibl--Dl7sw7RdkPgKiHQ"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
