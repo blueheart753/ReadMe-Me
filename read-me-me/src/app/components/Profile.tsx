52f5f0512a3c084b0a15529d328c6c2c4b0e8e7f
kimminam <bluemind8520@gmail.com>
2024-06-22 00:17:13 +0900

    Feat. className description -> onlyDescriptionBox

--- read-me-me/src/app/components/Profile.tsx
+++ read-me-me/src/app/components/Profile.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image';
-import profileImage from '../Images/Profile.jpg';
-import styles from '../page.module.css';
+import Image from 'next/image'
+import profileImage from '../Images/Profile.jpg'
+import styles from '../page.module.css'
 
 export interface PageTitleContentProps {
-  title: string;
-  subTitle?: string;
-  profileImage: string;
-  myJob: string;
+  title: string
+  subTitle?: string
+  profileImage: string
+  myJob: string
 }
 
 const Profile = () => {
@@ -18,14 +18,14 @@ const Profile = () => {
           <p className={styles.title}>Info.</p>
           <p className={styles.info}>간단한 정보.</p>
         </div>
-        <div className={styles.descriptionBox}>
+        <div className={styles.onlyDescriptionBox}>
           <p className={styles.description}>김미남(Kim minam)</p>
           <p className={styles.description}>kimminam8520@gmail.com</p>
           <p className={styles.description}>서울디지텍고등학교 재학 중</p>
         </div>
       </div>
     </article>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
